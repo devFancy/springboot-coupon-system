122a0f0c5423a4029706e33cdd9f1349ad972325
devfancy <jymoon1121@gmail.com>
2025-04-29 01:22:29 +0900

    feat(user): add Username and Password value objects for user entity

---
+++ coupon/coupon-api/src/main/java/dev/be/coupon/api/user/domain/vo/Username.java
@@ -0,0 +1,31 @@
+package dev.be.coupon.api.user.domain.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import static java.util.Objects.isNull;
+
+@Embeddable
+public class Username {
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    protected Username() {
+    }
+
+    public Username(final String username) {
+        validate(username);
+        this.username = username;
+    }
+
+    private String validate(final  String value) {
+        if (isNull(value) || value.isBlank()) {
+            throw new RuntimeException("사용자의 이름이 존재해야 합니다.");
+        }
+        return value;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
