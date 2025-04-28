954c2b7ecf92d44a248912ccc7ce85321d5d5030
devfancy <jymoon1121@gmail.com>
2025-04-29 01:20:42 +0900

    chore: modify package and class name

--- coupon/coupon-api/src/main/java/dev/be/coupon/CouponApplication.java
+++ coupon/coupon-api/src/main/java/dev/be/coupon/CouponApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreApplication {
+public class CouponApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
+        SpringApplication.run(CouponApplication.class, args);
     }
 
 }
--- coupon/coupon-api/src/main/java/dev/be/coupon/api/exceptionhanlder/GlobalExceptionHandler.java
+++ coupon/coupon-api/src/main/java/dev/be/coupon/api/exceptionhanlder/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package dev.be.coupon.api.exceptionhanlder;
 
-import dev.be.coupon.api.support.error.CoreException;
+import dev.be.coupon.api.support.error.CouponException;
 import dev.be.coupon.api.support.error.ErrorType;
 import dev.be.coupon.api.support.response.CommonResponse;
 import org.slf4j.Logger;
@@ -15,12 +15,12 @@ public class GlobalExceptionHandler {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @ExceptionHandler(CoreException.class)
-    public ResponseEntity<CommonResponse<?>> handleCoreException(CoreException e) {
+    @ExceptionHandler(CouponException.class)
+    public ResponseEntity<CommonResponse<?>> handleCouponException(CouponException e) {
         switch (e.getErrorType().getLogLevel()) {
-            case ERROR -> log.error("CoreException: {}", e.getMessage(), e);
-            case WARN -> log.warn("CoreException: {}", e.getMessage(), e);
-            default -> log.info("CoreException: {}", e.getMessage(), e);
+            case ERROR -> log.error("CouponException: {}", e.getMessage(), e);
+            case WARN -> log.warn("CouponException: {}", e.getMessage(), e);
+            default -> log.info("CouponException: {}", e.getMessage(), e);
         }
         return new ResponseEntity<>(CommonResponse.error(e.getErrorType(), e.getData()), e.getErrorType().getStatus());
     }
