483e695acf2e19ce9a8c128092afca8b493c9bda
devfancy <jymoon1121@gmail.com>
2025-05-04 00:02:53 +0900

    test(coupon-api): add UserServiceTest about sign-up

--- coupon/coupon-api/src/test/java/dev/be/coupon/api/user/application/UserServiceTest.java
+++ coupon/coupon-api/src/test/java/dev/be/coupon/api/user/application/UserServiceTest.java
@@ -3,10 +3,13 @@ package dev.be.coupon.api.user.application;
 import dev.be.coupon.api.user.application.dto.UserLoginCommand;
 import dev.be.coupon.api.user.application.dto.UserLoginResult;
 import dev.be.coupon.api.user.application.dto.UserSignUpCommand;
+import dev.be.coupon.api.user.application.dto.UserSignUpResult;
 import dev.be.coupon.api.user.domain.UserRepository;
 import dev.be.coupon.api.user.domain.exception.InvalidUserException;
 import dev.be.coupon.api.user.domain.vo.PasswordHasher;
 import dev.be.coupon.api.user.infrastructure.FakePasswordHasherClient;
+import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -26,6 +29,52 @@ class UserServiceTest {
         userRepository = new InMemoryUserRepository();
         passwordHasher = new FakePasswordHasherClient();
         userService = new UserService(userRepository, passwordHasher);
+    }
+
+    @DisplayName("아이디와 비밀번호를 입력하면 정상적으로 회원가입 성공한다.")
+    @Test
+    void success_signUp() {
+        // given
+        final UserSignUpCommand expected = new UserSignUpCommand("user1", "password1234");
+
+        // when
+        final UserSignUpResult actual = userService.signUp(expected);
+
+        // then
+        assertThat(actual.username()).isEqualTo(expected.username());
+    }
+
+    @DisplayName("같은 아이디로 회원가입을 시도하면 예외가 발생한다.")
+    @Test
+    void signUp_should_throw_exception_when_user_input_same_username() {
+        // given
+        final UserSignUpCommand first = new UserSignUpCommand("user1", "password1234");
+        final UserSignUpCommand duplicate = new UserSignUpCommand("user1", "password1234");
+
+        // when
+        userService.signUp(first);
+
+        // then
+        Assertions.assertThatThrownBy(() -> userService.signUp(duplicate))
+                .isInstanceOf(InvalidUserException.class)
+                .hasMessage("사용자 관련 부분에서 예외가 발생했습니다.");
+    }
+
+    @DisplayName("아이디와 비밀번호를 입력하면 정상적으로 로그인을 성공한다.")
+    @Test
+    void success_login() {
+        // given
+        UserSignUpCommand expected = new UserSignUpCommand("user1", "password1234");
+        userService.signUp(expected);
+
+        UserLoginCommand actual = new UserLoginCommand("user1", "password1234");
+
+        // when
+        UserLoginResult result = userService.login(actual);
+
+        // then
+        assertNotNull(result);
+        assertEquals(expected.username(), result.username());
     }
 
     @DisplayName("존재하지 않는 아이디와 비밀번호를 입력하면 예외가 발생한다.")
@@ -53,22 +102,5 @@ class UserServiceTest {
                 .isInstanceOf(InvalidUserException.class)
                 .hasMessage("사용자 관련 부분에서 예외가 발생했습니다.");
         assertNotEquals(actual.password(), expected.password());
-    }
-
-    @DisplayName("아이디와 비밀번호를 입력하면 정상적으로 로그인을 성공한다.")
-    @Test
-    void login_success() {
-        // given
-        UserSignUpCommand expected = new UserSignUpCommand("user1", "password1234");
-        userService.signUp(expected);
-
-        UserLoginCommand actual = new UserLoginCommand("user1", "password1234");
-
-        // when
-        UserLoginResult result = userService.login(actual);
-
-        // then
-        assertNotNull(result);
-        assertEquals(expected.username(), result.username());
     }
 }
