5006787bfdbaf161352d706556b2a35df7b35fd9
devfancy <jymoon1121@gmail.com>
2025-05-06 19:15:01 +0900

    refactor(coupon-api): apply DIP
    - by introducing UserRoleChecker to decouple auth dependency
    - order: application -> domain -> infrastructure

--- coupon/coupon-api/src/main/java/dev/be/coupon/api/coupon/application/CouponService.java
+++ coupon/coupon-api/src/main/java/dev/be/coupon/api/coupon/application/CouponService.java
@@ -4,20 +4,33 @@ import dev.be.coupon.api.coupon.application.dto.CouponCreateCommand;
 import dev.be.coupon.api.coupon.application.dto.CouponCreateResult;
 import dev.be.coupon.api.coupon.domain.Coupon;
 import dev.be.coupon.api.coupon.domain.CouponRepository;
+import dev.be.coupon.api.coupon.domain.UserRoleChecker;
+import dev.be.coupon.api.coupon.domain.exception.UnauthorizedAccessException;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class CouponService {
 
     private final CouponRepository couponRepository;
+    private final UserRoleChecker userRoleChecker;
 
-    public CouponService(final CouponRepository couponRepository) {
+    public CouponService(final CouponRepository couponRepository,
+                         final UserRoleChecker userRoleChecker) {
         this.couponRepository = couponRepository;
+        this.userRoleChecker = userRoleChecker;
     }
 
-    public CouponCreateResult create(final CouponCreateCommand command) {
-        final Coupon coupon = command.toDomain();
+    public CouponCreateResult create(
+            final UUID loginUserId,
+            final CouponCreateCommand command) {
 
+        if (!userRoleChecker.isAdmin(loginUserId)) {
+            throw new UnauthorizedAccessException("쿠폰 생성은 관리자만 가능합니다.");
+        }
+
+        final Coupon coupon = command.toDomain();
         return CouponCreateResult.from(couponRepository.save(coupon));
     }
 }
