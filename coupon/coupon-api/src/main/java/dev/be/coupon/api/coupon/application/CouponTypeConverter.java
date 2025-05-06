39874daa556dbf43262c4e57864561c987cacba6
devfancy <jymoon1121@gmail.com>
2025-05-06 19:16:40 +0900

    refactor(coupon-api): add validation to coupon type

--- coupon/coupon-api/src/main/java/dev/be/coupon/api/coupon/application/CouponTypeConverter.java
+++ coupon/coupon-api/src/main/java/dev/be/coupon/api/coupon/application/CouponTypeConverter.java
@@ -6,6 +6,10 @@ import dev.be.coupon.api.coupon.domain.exception.InvalidCouponTypeException;
 public class CouponTypeConverter {
 
     public static CouponType from(final String type) {
+        if (type == null || type.isBlank()) {
+            throw new InvalidCouponTypeException("쿠폰 타입이 null이거나 비어있습니다.");
+        }
+
         try {
             return CouponType.valueOf(type.toUpperCase());
         } catch (IllegalArgumentException e) {
