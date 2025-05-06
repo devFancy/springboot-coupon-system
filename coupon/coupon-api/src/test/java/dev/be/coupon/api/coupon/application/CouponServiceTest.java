90779c669df76ee8a96b3e403d62d322a060030a
devfancy <jymoon1121@gmail.com>
2025-05-07 01:58:44 +0900

    test(coupon-api): add unit tests for Redis-based locking logic

--- coupon/coupon-api/src/test/java/dev/be/coupon/api/coupon/application/CouponServiceTest.java
+++ coupon/coupon-api/src/test/java/dev/be/coupon/api/coupon/application/CouponServiceTest.java
@@ -16,7 +16,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
