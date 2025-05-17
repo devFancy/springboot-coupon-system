c368fe30513666e2f52103c1cab1328e337de1a1
devfancy <fancy.junyongmoon@gmail.com>
2025-05-17 22:05:01 +0900

    chore(coupon-api): add swagger docs about coupon usage api

--- coupon/coupon-api/src/main/java/dev/be/coupon/api/coupon/presentation/CouponControllerDocs.java
+++ coupon/coupon-api/src/main/java/dev/be/coupon/api/coupon/presentation/CouponControllerDocs.java
@@ -5,6 +5,7 @@ import dev.be.coupon.api.auth.presentation.dto.LoginUser;
 import dev.be.coupon.api.coupon.presentation.dto.CouponCreateRequest;
 import dev.be.coupon.api.coupon.presentation.dto.CouponCreateResponse;
 import dev.be.coupon.api.coupon.presentation.dto.CouponIssueResponse;
+import dev.be.coupon.api.coupon.presentation.dto.CouponUsageResponse;
 import dev.be.coupon.common.support.response.CommonResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -28,7 +29,7 @@ import java.util.UUID;
 public interface CouponControllerDocs {
 
     @Operation(
-            summary = "쿠폰 생성 성공",
+            summary = "쿠폰 생성",
             description = "관리자 권한이 있는 계정만 쿠폰을 생성할 수 있다."
     )
     @ApiResponses({
@@ -40,13 +41,26 @@ public interface CouponControllerDocs {
     );
 
     @Operation(
-            summary = "쿠폰 발급 성공",
+            summary = "쿠폰 발급",
             description = "사용자는 1번만 쿠폰을 발급할 수 있다."
     )
     @ApiResponses({
             @ApiResponse(responseCode = "201", description = "쿠폰 발급 성공"),
     })
     ResponseEntity<CommonResponse<CouponIssueResponse>> issue(
+            @Parameter(hidden = true) @AuthenticationPrincipal final LoginUser loginUser,
+            @PathVariable final UUID couponId
+    );
+
+
+    @Operation(
+            summary = "쿠폰 사용",
+            description = "사용자는 쿠폰을 사용할 수 있다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "쿠폰 사용 성공"),
+    })
+    ResponseEntity<CommonResponse<CouponUsageResponse>> usage(
             @Parameter(hidden = true) @AuthenticationPrincipal final LoginUser loginUser,
             @PathVariable final UUID couponId
     );
