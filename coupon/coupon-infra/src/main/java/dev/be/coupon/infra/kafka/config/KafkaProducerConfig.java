a9e2b96644ffb6b9f6e0f2971e0f65534d7f7782
devfancy <fancy.junyongmoon@gmail.com>
2025-06-26 17:31:19 +0900

    feat: add 'coupon-infra' module and move on api to infra module

---
+++ coupon/coupon-infra/src/main/java/dev/be/coupon/infra/kafka/config/KafkaProducerConfig.java
@@ -0,0 +1,34 @@
+package dev.be.coupon.infra.kafka.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    // 카프카 토픽에 데이터를 전송하기 위해 사용할 Kafka Template을 생성
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
---
+++ coupon/coupon-infra/src/main/java/dev/be/coupon/infra/kafka/producer/CouponIssueProducer.java
@@ -0,0 +1,41 @@
+package dev.be.coupon.infra.kafka.producer;
+
+import dev.be.coupon.infra.kafka.dto.CouponIssueMessage;
+import org.slf4j.MDC;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * CouponIssueProducer
+ * <p>
+ * 쿠폰 발급 요청을 Kafka 토픽("coupon_issue")으로 비동기 전송합니다.
+ * 이 메시지는 coupon-kafka-consumer 모듈의 CouponIssueConsumer 가 수신하여 DB에 저장합니다.
+ */
+@Component
+public class CouponIssueProducer {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private static final String GLOBAL_TRACE_ID_HEADER = "globalTraceId";
+
+    public CouponIssueProducer(final KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void issue(final UUID userId, final UUID couponId) {
+        CouponIssueMessage payload = new CouponIssueMessage(userId, couponId);
+        String globalTraceId = MDC.get("globalTraceId");
+
+        Message<CouponIssueMessage> message = MessageBuilder
+                .withPayload(payload)
+                .setHeader(KafkaHeaders.TOPIC, "coupon_issue")
+                .setHeader(GLOBAL_TRACE_ID_HEADER, globalTraceId)
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
