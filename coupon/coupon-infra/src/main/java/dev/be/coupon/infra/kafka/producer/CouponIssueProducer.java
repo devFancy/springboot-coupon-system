ede560659454bac329fe21ab5ccd93064b015d7e
devfancy <fancy.junyongmoon@gmail.com>
2025-07-23 22:02:19 +0900

    refactor: 메시지 전송을 파이어 앤 포겟에서 비동기로 전환
    - send() 메서드에 콜백 함수를 함께 전달

--- coupon/coupon-infra/src/main/java/dev/be/coupon/infra/kafka/producer/CouponIssueProducer.java
+++ coupon/coupon-infra/src/main/java/dev/be/coupon/infra/kafka/producer/CouponIssueProducer.java
@@ -1,14 +1,18 @@
 package dev.be.coupon.infra.kafka.producer;
 
 import dev.be.coupon.infra.kafka.dto.CouponIssueMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * CouponIssueProducer
@@ -21,6 +25,7 @@ public class CouponIssueProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
     private static final String GLOBAL_TRACE_ID_HEADER = "globalTraceId";
+    private static final Logger log = LoggerFactory.getLogger(CouponIssueProducer.class);
 
     public CouponIssueProducer(final KafkaTemplate<String, Object> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -36,6 +41,22 @@ public class CouponIssueProducer {
                 .setHeader(GLOBAL_TRACE_ID_HEADER, globalTraceId)
                 .build();
 
-        kafkaTemplate.send(message);
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send(message);
+        future.whenComplete((result, exception) -> {
+            if (exception == null) {
+                log.info("메시지 전송 성공. GlobalTraceId: {}, Topic: {}, Partition: {}, Offset: {}, Payload: {}",
+                        globalTraceId,
+                        result.getRecordMetadata().topic(),
+                        result.getRecordMetadata().partition(),
+                        result.getRecordMetadata().offset(),
+                        payload);
+            } else {
+                log.error("메시지 전송 실패 GlobalTraceId: {}, Payload: {}",
+                        globalTraceId,
+                        payload,
+                        exception);
+                // 여기에 실패 시 필요한 추가 로직을 구현 (e.g., 재시도, 알림, DB에 실패 기록 등)
+            }
+        });
     }
 }
