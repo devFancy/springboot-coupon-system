5c206d5c28703528c8645bcce7e09bed55533cff
devfancy <fancy.junyongmoon@gmail.com>
2025-07-29 00:43:49 +0900

    feat(consumer, infra): kafka topic 및 consumer 설정 강화

--- coupon/coupon-consumer/src/main/java/dev/be/coupon/kafka/consumer/config/KafkaConsumerConfig.java
+++ coupon/coupon-consumer/src/main/java/dev/be/coupon/kafka/consumer/config/KafkaConsumerConfig.java
@@ -30,7 +30,9 @@ public class KafkaConsumerConfig {
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
 
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
+        // 오프셋 자동 커밋을 비활성화 -> 메시지 처리 완료 후 애플리케이션이 직접 커밋 시점을 제어하여, 메시지 유실을 방지
         config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        // 한 번의 poll() 호출로 가져올 최대 레코드 수를 지정
         config.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100);
 
         return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), deserializer);
@@ -40,9 +42,9 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, CouponIssueMessage> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, CouponIssueMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // 리스너에서 Acknowledgment.acknowledge()를 명시적으로 호출해야만 오프셋이 커밋합니다.
         factory.getContainerProperties().setObservationEnabled(true); // Micrometer 연동 활성화
-        factory.setConcurrency(1); // Kafka 파티션 수와 동일하게 설정
+        factory.setConcurrency(3); // 토픽의 파티션 수와 일치시켜 병렬 처리 성능을 최적화합니다.
         return factory;
     }
 }
