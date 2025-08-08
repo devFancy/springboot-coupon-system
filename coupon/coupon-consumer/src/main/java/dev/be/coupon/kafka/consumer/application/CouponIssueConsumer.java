e5a8a8a3e5df95894d52fd01e8be5345aaee4cd8
devfancy <fancy.junyongmoon@gmail.com>
2025-08-08 20:39:52 +0900

    refactor(consumer): CouponIssueConsumer의 수동 MDC 로직 제거

--- coupon/coupon-consumer/src/main/java/dev/be/coupon/kafka/consumer/application/CouponIssueConsumer.java
+++ coupon/coupon-consumer/src/main/java/dev/be/coupon/kafka/consumer/application/CouponIssueConsumer.java
@@ -3,13 +3,10 @@ package dev.be.coupon.kafka.consumer.application;
 import dev.be.coupon.infra.kafka.dto.CouponIssueMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
 
 /**
  * "coupon_issue" Kafka 토픽으로부터 쿠폰 발급 메시지를 수신하여 발급 쿠폰을 DB에 저장합니다.
@@ -19,7 +16,6 @@ public class CouponIssueConsumer {
 
     private final CouponIssuanceService couponIssuanceService;
     private final Logger log = LoggerFactory.getLogger(CouponIssueConsumer.class);
-    private static final String GLOBAL_TRACE_ID_KEY = "globalTraceId";
 
     public CouponIssueConsumer(final CouponIssuanceService couponIssuanceService) {
         this.couponIssuanceService = couponIssuanceService;
@@ -32,12 +28,7 @@ public class CouponIssueConsumer {
      */
     @KafkaListener(topics = "#{T(dev.be.coupon.infra.kafka.KafkaTopic).COUPON_ISSUE.getTopicName()}", groupId = "group_1")
     public void listener(final CouponIssueMessage message,
-                         @Header(name = GLOBAL_TRACE_ID_KEY, required = false) final String globalTraceId,
                          final Acknowledgment ack) {
-
-        if (!Objects.isNull(globalTraceId)) {
-            MDC.put(GLOBAL_TRACE_ID_KEY, globalTraceId);
-        }
         log.info("발급 처리 메시지 수신: {}", message);
 
         try {
@@ -45,8 +36,6 @@ public class CouponIssueConsumer {
             ack.acknowledge();
         } catch (Exception e) {
             log.error("메시지 처리 실패, 재처리를 위해 커밋하지 않음: {}", message, e);
-        } finally {
-            MDC.remove(GLOBAL_TRACE_ID_KEY);
         }
     }
 }
