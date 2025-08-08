78cecbae04442f72b72ea2c376c969a61a0a38a5
devfancy <fancy.junyongmoon@gmail.com>
2025-08-08 20:36:36 +0900

    feat(infra): KafkaProducerConfig에 프로듀서 인터셉터 등록

--- coupon/coupon-infra/src/main/java/dev/be/coupon/infra/kafka/config/KafkaProducerConfig.java
+++ coupon/coupon-infra/src/main/java/dev/be/coupon/infra/kafka/config/KafkaProducerConfig.java
@@ -1,5 +1,6 @@
 package dev.be.coupon.infra.kafka.config;
 
+import dev.be.coupon.infra.kafka.interceptor.MdcProducerInterceptor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -48,6 +49,9 @@ public class KafkaProducerConfig {
 
         // 압축 타입 설정: 네트워크 대역폭과 저장 공간을 절약하기 위해 메시지 압축을 사용합니다.
         config.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
+        // 인터셉터 설정
+        config.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, MdcProducerInterceptor.class.getName());
 
         return new DefaultKafkaProducerFactory<>(config);
     }
