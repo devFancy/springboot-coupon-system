66103cc42a2e3f0957559b597f8b586419e84f54
devfancy <fancy.junyongmoon@gmail.com>
2025-09-02 13:08:38 +0900

    config(api, consumer): 로컬과 테스트 환경 분리

--- coupon/coupon-api/src/test/java/dev/be/coupon/api/user/application/UserServiceTest.java
+++ coupon/coupon-api/src/test/java/dev/be/coupon/api/user/application/UserServiceTest.java
@@ -11,7 +11,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 class UserServiceTest {
 
     private UserService userService;
