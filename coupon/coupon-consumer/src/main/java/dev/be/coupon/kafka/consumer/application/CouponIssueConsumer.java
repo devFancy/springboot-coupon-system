536eecf8dd378b9df3c787aebe9698c2954fac42
Junyong Moon <fancy.junyongmoon@gmail.com>
2025-10-31 15:33:23 +0900

    refactor: 컨슈머 서버에서 쿠폰 발급에 대한 장애 처리 로직을 FailureHandler로 분리 및 개선 (#57)
    * refactor(consumer): 컨슈머 장애 처리를 CouponIssueFailureHandler 로 분리
    
    * test(consumer): CouponIssueFailureHandler 통합 테스트코드 작성
    
    * test(consumer): 기존 CouponIssueConsumerTest 통합 테스코드 수정
    
    - 중복된 테스트 케이스 제거
    
    * chore: version 1.4.6 로 업데이트

--- coupon/coupon-consumer/src/main/java/dev/be/coupon/kafka/consumer/application/CouponIssueConsumer.java
+++ coupon/coupon-consumer/src/main/java/dev/be/coupon/kafka/consumer/application/CouponIssueConsumer.java
@@ -1,9 +1,6 @@
 package dev.be.coupon.kafka.consumer.application;
 
-import dev.be.coupon.domain.coupon.exception.CouponDomainException;
 import dev.be.coupon.infra.kafka.dto.CouponIssueMessage;
-import dev.be.coupon.kafka.consumer.support.error.CouponConsumerException;
-import dev.be.coupon.kafka.consumer.support.error.ErrorType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,16 +12,13 @@ import org.springframework.stereotype.Component;
 public class CouponIssueConsumer {
 
     private final CouponIssuanceFacade couponIssuanceFacade;
-    private final CouponIssuanceService couponIssuanceService;
-    private final FailedCouponRetryCountIncrease failedCouponRetryCountIncrease;
+    private final CouponIssueFailureHandler couponIssueFailureHandler;
     private final Logger log = LoggerFactory.getLogger(CouponIssueConsumer.class);
 
     public CouponIssueConsumer(final CouponIssuanceFacade couponIssuanceFacade,
-                               final CouponIssuanceService couponIssuanceService,
-                               final FailedCouponRetryCountIncrease failedCouponRetryCountIncrease) {
+                               final CouponIssueFailureHandler couponIssueFailureHandler) {
         this.couponIssuanceFacade = couponIssuanceFacade;
-        this.couponIssuanceService = couponIssuanceService;
-        this.failedCouponRetryCountIncrease = failedCouponRetryCountIncrease;
+        this.couponIssueFailureHandler = couponIssueFailureHandler;
     }
 
     /**
@@ -35,47 +29,24 @@ public class CouponIssueConsumer {
     @KafkaListener(topics = "${kafka.topic.coupon-issue}", groupId = "group_1")
     public void listener(final CouponIssueMessage message,
                          final Acknowledgment ack) {
-        log.info("발급 처리 메시지 수신: {}", message);
-
+        log.info("[CouponIssueConsumer_listener] 발급 처리 메시지 수신: userId={}, couponId={}", message.userId(), message.couponId());
         try {
             couponIssuanceFacade.process(message);
             ack.acknowledge();
-        } catch (CouponDomainException | CouponConsumerException e) {
-            log.info("[CouponIssueConsumer_listener] 비즈니스 예외 발생. 메시지 폐기. ack: {} (메시지: {})", e.getMessage(), message);
-            ack.acknowledge();
         } catch (Exception e) {
-            log.error("[CouponIssueConsumer_listener] 인프라 예외 발생. 실패 이력 저장 시도...: {} (메시지: {})", e.getMessage(), message, e);
-            try {
-                couponIssuanceService.recordFailure(message.userId(), message.couponId());
-                ack.acknowledge();
-            } catch (Exception dbFail) {
-                log.error("[CouponIssueConsumer_listener] DB 완전 장애! 실패 기록조차 실패. nack()로 Kafka 재시도 유도", dbFail);
-                throw new CouponConsumerException(ErrorType.COUPON_ISSUANCE_FAILED);
-            }
+            couponIssueFailureHandler.handle(message, ack, e);
         }
     }
 
     @KafkaListener(topics = "${kafka.topic.coupon-issue-retry}", groupId = "group_1")
     public void listenerRetry(final CouponIssueMessage message,
                          final Acknowledgment ack) {
-        log.info("발급 처리 메시지 수신: {}", message);
-
+        log.info("[CouponIssueConsumer_listenerRetry] 재시도 발급 처리 메시지 수신: userId={}, couponId={}", message.userId(), message.couponId());
         try {
             couponIssuanceFacade.processRetry(message);
             ack.acknowledge();
-        } catch (CouponDomainException | CouponConsumerException e) {
-            log.warn("[CouponIssueConsumer_listenerRetry] 재시도 중 비즈니스 예외 발생. 재시도 횟수 증가 후 포기. ack: {} (메시지: {})", e.getMessage(), message);
-            failedCouponRetryCountIncrease.retryCountIncrease(message.failedIssuedCouponId());
-            ack.acknowledge();
         } catch (Exception e) {
-            log.error("[CouponIssueConsumer_listenerRetry] 재시도 중 인프라 예외 발생. 재시도 횟수 증가 시도...: {} (메시지: {})", e.getMessage(), message, e);
-            try {
-                failedCouponRetryCountIncrease.retryCountIncrease(message.failedIssuedCouponId());
-                ack.acknowledge();
-            } catch (Exception dbFail) {
-                log.error("[CouponIssueConsumer_listenerRetry] DB 완전 장애! 횟수 증가조차 실패. nack()로 Kafka 재시도 유도", dbFail);
-                throw new CouponConsumerException(ErrorType.COUPON_ISSUANCE_RETRY_FAILED);
-            }
+            couponIssueFailureHandler.handleRetry(message, ack, e);
         }
     }
 }
