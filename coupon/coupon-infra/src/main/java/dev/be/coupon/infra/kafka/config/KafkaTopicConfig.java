7f83d25bf59efbf368790ac41b86f32ceefaca62
devfancy <fancy.junyongmoon@gmail.com>
2025-11-27 16:08:05 +0900

    chore(infra): topic에 대한 파티션 3개로 통일

--- coupon/coupon-infra/src/main/java/dev/be/coupon/infra/kafka/config/KafkaTopicConfig.java
+++ coupon/coupon-infra/src/main/java/dev/be/coupon/infra/kafka/config/KafkaTopicConfig.java
@@ -13,16 +13,16 @@ public class KafkaTopicConfig {
     @Bean
     public NewTopic couponIssueTopic(@Value("${kafka.topic.coupon-issue}") final String topic) {
         return TopicBuilder.name(topic)
-                .partitions(3)      // 파티션 수
-                .replicas(1)        // 운영 환경에서는 장애 허용과 고가용성을 위해 3 이상으로 설정, 각 파티션마다 1개의 리더와 2개의 팔로워로 구성됨
-                .config(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "1") // 최소 2개 이상의 리플리카에 데이터가 복제되었을 때만 쓰기 성공으로 간주하여 데이터 유실 방지
+                .partitions(3) // 파티션 수 >= 전체 컨슈머 수 (컨슈머가 파티션보다 많으면 나머지는 놉니다)
+                .replicas(1) // 운영 환경에서는 장애 허용과 고가용성을 위해 3 이상으로 설정, 각 파티션마다 1개의 리더와 2개의 팔로워로 구성됨
+                .config(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "1") // 운영 환경에서는 최소 2개 이상의 리플리카(리더, 팔로워)에 데이터가 복제되었을 때만 쓰기 성공(ack)으로 간주하여 데이터 유실 방지
                 .build();
     }
 
     @Bean
     public NewTopic couponIssueRetryTopic(@Value("${kafka.topic.coupon-issue-retry}") final String topic) {
         return TopicBuilder.name(topic)
-                .partitions(1)
+                .partitions(3)
                 .replicas(1)
                 .build();
     }
